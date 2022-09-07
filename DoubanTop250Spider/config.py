36489b5e803833331e6e6a3d85bb58733664cc11
Duguce <zhgyqc@163.com>
2022-09-07 21:43:06 +0800

    update README.md

--- DoubanTop250Spider/config.py
+++
@@ -1,8 +0,0 @@
-# Links of douban Top250.
-links = ['https://movie.douban.com/top250?start=' + str(i) + '&filter='.format(str(i)) for i in range(0, 250, 25)]
-
-# Name of the Excel file that stores the data.
-filename = 'douban_top250_data.xlsx'
-
-# Interval between requests.
-interval = 1
